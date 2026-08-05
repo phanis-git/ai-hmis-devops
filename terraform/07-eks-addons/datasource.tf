69d5c2382211b0e025b4f7bd89c7c49253820d78
phanis-git <phanipspkmec@gmail.com>
2026-08-05 17:13:59 +0530

    corrections eks-addons

--- terraform/07-eks-addons/datasource.tf
+++ terraform/07-eks-addons/datasource.tf
@@ -1,12 +1,22 @@
 # data "aws_ssm_parameter" "cluster_name" {
 #   name = "${var.project_name}-${var.environment}-eks-cluster-name"
 # }
+# data "aws_eks_cluster" "eks" {
+#   name = local.cluster_name
+# }
+
+# data "aws_caller_identity" "current" {}
+
+# data "aws_iam_openid_connect_provider" "oidc" {
+#   url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+# }
 data "aws_eks_cluster" "eks" {
   name = local.cluster_name
 }
 
-data "aws_caller_identity" "current" {}
-
-data "aws_iam_openid_connect_provider" "oidc" {
+data "tls_certificate" "eks" {
   url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
 }
+# data "aws_iam_openid_connect_provider" "oidc" {
+#   url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+# }
--- terraform/07-eks-addons/main.tf
+++ terraform/07-eks-addons/main.tf
@@ -70,7 +70,8 @@ data "aws_iam_policy_document" "ebs_csi_assume_role" {
       type = "Federated"
 
       identifiers = [
-        data.aws_iam_openid_connect_provider.oidc.arn
+        # data.aws_iam_openid_connect_provider.oidc.arn
+         aws_iam_openid_connect_provider.eks.arn
       ]
     }
 
@@ -101,4 +102,16 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
   role = aws_iam_role.ebs_csi_role.name
 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+resource "aws_iam_openid_connect_provider" "eks" {
+
+  url = data.aws_eks_cluster.eks.identity[0].oidc[0].issuer
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    data.tls_certificate.eks.certificates[0].sha1_fingerprint
+  ]
 }
